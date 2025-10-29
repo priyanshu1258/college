d3b749513905236a26ec74b8aeeaa8d3adbf914f
Priyanshu Attri <priyanshuattri05@gmail.com>
2025-10-29 14:20:31 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -118,8 +118,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: [
-      process.env.FRONTEND_URL || "http://localhost:5173",
-      "http://localhost:5174",
+      process.env.FRONTEND_URL,
     ],
     credentials: true,
   })
